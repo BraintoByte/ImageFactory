ac72a7e845163fb0f3ee3f72fab044f060f9f50d
braintobyte <gaspari4@gmail.com>
2018-07-18 02:56:12 -0700

    AlphaBranch

--- src/com/braintobyte/imagefactory/main/Driver.java
+++ src/com/braintobyte/imagefactory/main/Driver.java
@@ -20,58 +20,11 @@ import com.braintobyte.imagefactory.factoryUtils.UtilsGui;
 import com.braintobyte.imagefactory.frames.Frame;
 import com.braintobyte.imagefactory.pannels.TranslucentPannel;
 
+
+//V_1.0-Alpha
+
+
 public class Driver {
 	public static void main(String[] args) {
-		
-		
-//		BufferedImage imageRotation = ImageUtils.rotateImage(ImageUtils.loadImage(System.getProperty("user.dir") + "\\ImagesTest\\SecondPart.png"), 20);
-//		
-//		try {
-//			ImageIO.write(imageRotation, "PNG", new File("K:\\EGit\\ImageFactory_V1.0\\ImagesTest\\", "test.png"));
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		BufferedImage image = ImageUtils.mergeImages(System.getProperty("user.dir") + "\\ImagesTest\\FirstPart.png", System.getProperty("user.dir") + "\\ImagesTest\\SecondPart.png");
-//		
-//		try {
-//			ImageIO.write(image, "PNG", new File("K:\\EGit\\ImageFactory_V1.0\\ImagesTest\\", "test.png"));
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-//		TranslucentPannel pannel = new TranslucentPannel(200, 200);
-//		pannel.setImageBackground(new ImageIcon("/home/law/git/ImageFactory_V1.0/ImagesTest/machine.png").getImage());
-//		Frame frame = new Frame(0, 0, 500, 500);
-//		
-//		frame.setSize(400, 400);
-//		frame.setLocationRelativeTo(null);
-//		frame.setBgColor(Color.RED);
-//		pannel.setVisible(true);
-//		frame.addPannel(pannel);
-//		frame.pack();
-//		frame.setVisible(true);
-//		
-//		JFrame frame2 = new JFrame("");
-//		JButton button = new JButton("click!");
-//		
-//		button.addActionListener(new ActionListener() {
-//			
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				frame.setDecorated(!frame.isDecorated());
-//			}
-//		});
-//		
-//		button.setFont(UtilsGui.loadFont(System.getProperty("user.dir") + File.separator + "fonts" + File.separator + "android.ttf", 6));
-//		
-//		frame2.setSize(200, 200);
-//		frame2.setLocationRelativeTo(null);
-//		frame2.add(button);
-//		frame2.pack();
-//		frame2.setVisible(true);
-		
 	}
 }
